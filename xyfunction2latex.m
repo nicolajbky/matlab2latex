2c48f9c3bae880ba3f3085933ab742bcf646875c
Nicolaj <nicolaj.baramsky@gmail.com>
2017-05-02 10:40:08 +0200

    added resample

--- xyfunction2latex.m
+++ xyfunction2latex.m
@@ -1,7 +1,12 @@
-function str = xyfunction2latex(x,y)
+function str = xyfunction2latex(x,y,f)
 %function str = xyfunction2latex(x,y)
-%   converts the data from x and y into LaTeX form to be pasted.
+%  converts the data from x and y into latex form to be pasted.
 %  x and y must have same length
+%  if option f is given, the data is resampled with a frequency of f Hz
+
+if nargin > 2
+ [x,y] = resample(y,x,f);
+end
 
 str='\addplot[] coordinates{';
 for i=1:length(x)
